82bbb1dcb2d4199b6c275366d0622730a45b9ce3
AbhishaShah <abhisha5993@gmail.com>
2023-01-18 15:58:21 -0500

    02-started mini product catalog app

---
+++ 02-mini-product-catalog/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ProductListComponent } from '../lib/components/product-list';
+import { TopBarComponent } from '../lib/components/top-bar';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent
+    ],
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule { }
---
+++ 02-mini-product-catalog/src/lib/config/products.ts
@@ -0,0 +1,29 @@
+export interface Product {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+  }
+  
+  export const products = [
+    {
+      id: 1,
+      name: 'Classic wooden chair',
+      price: 500,
+      description: 'A classis comfy wooden chair'
+    },
+    {
+      id: 2,
+      name: 'Side table',
+      price: 250,
+      description: 'A great side table for decor'
+    },
+    {
+      id: 3,
+      name: 'Mirror',
+      price: 105,
+      description: 'A stylist mirror'
+    }
+  ];
+  
+  
