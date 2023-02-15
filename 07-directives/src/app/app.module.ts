113a25898a23a056922088319d87a43786816ad7
AbhishaShah <abhisha5993@gmail.com>
2023-02-15 13:58:21 -0500

    Learned about directives

--- 07-directives/src/app/app.module.ts
+++ 07-directives/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NgForComponent } from './ng-for/ng-for.component';
+import { NgSwitchComponent } from './ng-switch/ng-switch.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NgForComponent, NgSwitchComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
